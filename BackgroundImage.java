cd44524e3fd66f3df88816af24ab9ae1f7ab86a6
xxs12 <xxs12@DESKTOP-EVIM054.myfiosgateway.com>
2020-05-24 17:16:59 -0400

    please update

---
+++ BackgroundImage.java
@@ -0,0 +1,40 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Ben Dai, Leon Li, Eric Han, Phillip Zhang
+ * background
+ *
+ */
+public class BackgroundImage extends JPanel
+{
+
+	public BackgroundImage(int w, int h)
+	{
+		
+		setSize(w, h);
+		setLayout(null);
+		
+		try
+		{
+			
+			BufferedImage bImg = ImageIO.read(new File("NinjaBackground.jpg"));
+			JLabel label = new JLabel(new ImageIcon(bImg));
+			label.setBounds(0, 0, getWidth(), getHeight());
+			add(label);
+			
+		}
+		catch (IOException e) {}
+		
+		setVisible(true);
+		
+	}
+	
+}
--- GameFrame.java
+++ GameFrame.java
@@ -14,8 +14,11 @@ public class GameFrame extends JFrame
 		
 		setBounds(100, 100, 1400, 800);
 		setResizable(false);
+		setLayout(null);
 		
-		//to do
+		BackgroundImage background = new BackgroundImage(getWidth(), getHeight());
+		background.setLocation(0, 0);
+		add(background);
 		
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
