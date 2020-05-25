26e13ea7722cf92262d1d8a04368d75302032ef3
xxs12 <xxs12@DESKTOP-EVIM054.myfiosgateway.com>
2020-05-25 16:33:19 -0400

    dsadasdadsad

---
+++ Door.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Ben Dai, Leon Li, Eric Han, Phillip Zhang
+ * door
+ *
+ */
+public class Door extends Obstacles
+{
+	
+	public Door(int x, int y, int width, int height)
+	{
+		
+	}
+
+}
--- DoorWithButton.java
+++ DoorWithButton.java
@@ -1,10 +1,12 @@
+import javax.swing.JComponent;
+
 /**
  * 
  * @author Ben Dai, Leon Li, Eric Han, Phillip Zhang
  * door w/ button obstacle
  *
  */
-public class DoorWithButton extends Obstacles
+public class DoorWithButton extends JComponent
 {
 	public DoorWithButton(int x, int y, int width, int height)
 	{
